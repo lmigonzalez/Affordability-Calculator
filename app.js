c5d5a39598b55aa661a8dd3d9260110ac2c4bdb2
joszze <jossecarlos@gmail.com>
2023-03-21 21:13:45 -0400

    fix direction of condition

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ function calculateResult(
     (monthlyMortagePayment / (amortizedonstant(interestRateDecimalMonthly, loanTerm)))
   
   
-  const homePrice = (propertyPrice<downPayment?propertyPrice:0) + downPayment;
+  const homePrice = (propertyPrice>downPayment?propertyPrice:0) + downPayment;
 
   document.getElementById('result').textContent = parseInt(homePrice)
     .toString()
